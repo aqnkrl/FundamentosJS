af13baf603f711f93adb379ca4767edb463ef37c
karla.aquino <karlaaquino371@gmail.com>
2024-09-12 10:56:49 -0600

    01 Tipos de Dato en Javascript

---
+++ concepts/datatypes.js
@@ -0,0 +1,17 @@
+//Tipos de datos 
+console.log(24);
+console.log(typeof 24);
+console.log(2.3);
+console.log( typeof 2.3);
+console.log(true);
+console.log( typeof true);
+console.log("Hello mundo");
+console.log( typeof "Hello mundo");
+console.log (typeof null);
+console.log(typeof undefined);
+console.log([1, 2, 3]);
+console.log( typeof[1, 2, 3]);
+console.log({nombre: "Juan"});
+console.log( typeof {nombre: "Juan"});
+
+
