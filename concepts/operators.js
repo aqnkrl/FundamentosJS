e7f4d2a365326751eb2e113e1974f5fc0756ca41
karla.aquino <karlaaquino371@gmail.com>
2024-09-12 11:30:29 -0600

    02 variables let y const

---
+++ concepts/operators.js
@@ -0,0 +1,41 @@
+//Operadores Matemáticos
+// + y -
+
+console.log(10+3);
+console.log(10-3);
+console.log(10*3);
+console.log(10/3);
+console.log(10%3);
+console.log(10**3);
+
+//Operadores string
+//concatenación con +
+
+console.log("Hola" + "" + "Mundo");
+
+// operadores de asignación
+let a = 1;
+a += 3;
+a -= 3;
+a *= 3;
+a /= 3;
+a %= 3;
+a **= 3;
+
+console.log(a);
+
+//Operadores de comparación
+console.log(10 > 3);
+console.log(10 < 3);
+console.log(10 >= 3);
+console.log(10 <= 3);
+console.log(10 == 3);
+console.log(10 === 3);
+console.log(10 != 3);
+console.log(10 !== 3);
+
+//Operadores Lógicos 
+console.log(true && true)//and
+console.log(true|| true)//or
+console.log(!true)//not
+
---
+++ concepts/variables.js
@@ -0,0 +1,18 @@
+//Variables
+//Declaramos variables con let o const
+//let para variables que cambian de valor
+//const para variables que no cambian de valor
+
+let name;
+console.log(name);
+name = "Peter";
+console.log(name);
+console.log( typeof name);
+name=18;
+console.log(name);
+console.log(typeof name);
+
+const PI=3.1416;
+console.log(PI);
+console.log(typeof PI);
+PI=3.15;
